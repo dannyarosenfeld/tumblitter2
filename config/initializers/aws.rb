3c8e0e8a9074338f28a9de85ecaf74ec0c1ca15c
danny rosenfeld <dannyseniorproject@gmail.com>
2016-01-11 20:08:57 +0200

    changed aws.rb config initializer for deployment

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,12 @@
-AWS.config.update({
-  region: ENV['AWS_REGION'],
-  credentials: AWS::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-})
+
+
+#AWS.config.update({
+ # region: ENV['AWS_REGION'],
+  #credentials: AWS::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+#})
+
+AWS.config(:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
+
+s3 = AWS::S3.new
+AVATAR_BUCKET = s3.buckets[ENV['AVATAR_BUCKET_NAME']]
