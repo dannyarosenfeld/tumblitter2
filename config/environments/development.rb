a8e6efb148f36b7b3fff656ee66e30b5bfdf1eba
danny rosenfeld <dannyseniorproject@gmail.com>
2016-01-18 07:35:40 +0200

    invalid access key id error deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,10 +44,13 @@ Tumblful::Application.configure do
 
 config.paperclip_defaults = {
   :storage => :s3,
+  
   :s3_credentials => {
+    #:region => ENV['AWS_REGION'],
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :region => 'us-west-2'
   }
 }
 
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,7 @@
 
 
 #AWS.config.update({
-  #region: ENV['AWS_REGION'],
+ # region: ENV['AWS_REGION'],
  # credentials: AWS::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 #})
 
@@ -14,4 +14,9 @@
  #                     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
 
 #s3 = AWS::S3.new
+#S3::setEndpoint('s3-eu-west-1.amazonaws.com');
 #AVATAR_BUCKET = s3.buckets[ENV['AVATAR_BUCKET_NAME']]
+
+AWS.config(
+access_key_id: 'your_access_key',
+secret_access_key: 'your_secret_access_key')
